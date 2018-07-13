7693885e58ef78f3508cbaabeb158ad4e8643801
Shuhei Hiya <syuhei176@gmail.com>
2018-07-14 08:59:17 +0900

    Add SIGNER and SIGNER_PRIVATE_KEY env

--- web-dapp/server-config-private.example.js
+++ web-dapp/server-config-private.example.js
@@ -7,8 +7,8 @@ module.exports = function () {
         twilioAccountSid: process.env.TWILIO_ACCOUNT_SID,
         twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
         twilioFromNo: '+15005550006',
-        signer: '0xdbde11e51b9fcc9c455de9af89729cf37d835156',
-        signerPrivateKey: '1dd9083e16e190fa5413f87837025556063c546bf16e38cc53fd5d018a3acfbb',
+        signer: process.env.SIGNER,
+        signerPrivateKey: process.env.SIGNER_PRIVATE_KEY,
         lobApiVersion: '2018-03-01',
     };
 };
